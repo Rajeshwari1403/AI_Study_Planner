d6473f0782d27179f5050d59b606ac4b8bf6c2d3
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2026-03-04 21:49:32 +0530

    Initial COmmit

---
+++ frontend/ai_study_path/src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './pages/Auth/LoginPage.jsx'
+import RegisterPage from './pages/Auth/RegisterPage.jsx'
+import NotFoundpage from './pages/NotFoundPage'
+import ProtectedRoute from './components/auth/ProtectedRoute.jsx'
+import DashboardPage from './pages/Auth/DashBoardPage.jsx'
+import DocumentListPage from './pages/Documents/DocumentListPage.jsx'
+import DocumentDetailPage from './pages/Documents/DocumentsDetailsPage.jsx'
+import FlashcardListPage from './pages/Flashcard/FlashCardListPage.jsx'
+import FlashCardPage from './pages/Flashcard/FlashCardPage.jsx'
+import QuizzesPage from './pages/Quizzes/QuizzesPage.jsx'
+import QuizResultsPage from './pages/Quizzes/QuizResultPage.jsx'
+import ProfilePage from './pages/Profile/ProfilePage.jsx'
+
+const App = () => {
+  const isAuthenticated = true
+  const loading = false
+
+  if (loading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route 
+           path="/"
+           element = {isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}
+        />
+        <Route path="/login" element={< LoginPage />} />
+        <Route path="/register" element={< RegisterPage />} />
+
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/documents" element={<DocumentListPage />} />
+          <Route path="/documents/:id" element={<DocumentDetailPage />} />
+          <Route path="/flashcards" element={<FlashcardListPage />} />
+          <Route path="/documents/:id/flashcards" element={<FlashCardPage />} />
+          <Route path="/quizzes/:quizzId" element={<QuizzesPage />} />
+          <Route path="/quizzes/:quizzId/results" element={<QuizResultsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundpage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App

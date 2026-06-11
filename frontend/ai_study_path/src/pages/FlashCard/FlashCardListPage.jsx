e68ba145ab1dc526d9969c56609059f456100134
Rajeshwari1403 <rajeshwariravi2005@gmail.com>
2026-06-11 19:10:24 +0530

    Changes

--- frontend/ai_study_path/src/pages/FlashCard/FlashCardListPage.jsx
+++ frontend/ai_study_path/src/pages/FlashCard/FlashCardListPage.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import flashcardService from '../../services/flashcardService';
+import toast from 'react-hot-toast';
+import Spinner from '../../components/common/Spinner';
+import PageHeader from '../../components/common/PageHeader';
+import EmptyState from '../../components/common/EmptyState';
+import FlashcardSetCard from '../../components/flashcards/FlashcardSetCard'
 
 const FlashCardListPage = () => {
+  const [flashcardSets, setFlashcardSets] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchFlashcardSets = async () => {
+      try {
+        const response = await flashcardService.getAllFlashcardSets();
+        console.log("fetchFlashcardSets___", response.data);
+        setFlashcardSets(response.data);
+      } catch (error) {
+        toast.error('Failed to fetch flashcard sets.');
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFlashcardSets();
+  }, []);
+
+  const renderContent = () => {
+    if(loading) {
+      return <Spinner />
+    }
+
+    if(flashcardSets.length === 0) {
+      return (
+        <EmptyState 
+            title="No FlashCard Sets Found"
+            description="You have not generated any flashcard yet. Go to document to create your set."
+        />
+      );
+    }
+
+    return (
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+        {flashcardSets.map((set) => (
+          <FlashcardSetCard key={set._id} flashcardSet={set} />
+        ))}
+      </div>
+    );
+  };
+
   return (
-    <div>FlashCardListPage</div>
+    <div>
+      <PageHeader title="All Flashcard Sets" />
+      {renderContent()}
+    </div>
   )
 }
 
